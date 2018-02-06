31861e7e5eab9e85357e282f8b0cdaaa3104b1b7
DonVictory <vitory.dong@gmail.com>
2018-02-06 18:57:06 +0800

    重写update函数

--- web-page/mysqlModel.py
+++ web-page/mysqlModel.py
@@ -6,7 +6,7 @@ import time
 import pymysql
 __author__ = 'Drw'
 
-class mysqlModel(object):
+class mysqlmodel(object):
     # init
     def __init__(self,host,user,passwd,dbname,port = 3306,charset = 'utf8'):
         self.conn = pymysql.Connect(
@@ -31,10 +31,13 @@ class mysqlModel(object):
         selSql = "select %s from %s %s" % (field,tbName,where)
         return self.excute(selSql)
     # update
-    def update(self, field, value,tbName, where):
+    def update(self, keyValues,tbName, where):
+        setValue = ''
+        for k,v in keyValues.items():
+            setValue += '`%s`="%s",' % (k,v)
         if where:
             where = " where "+where
-        updateSql = "update %s set %s=%s %s" % (tbName,field,value,where)
+        updateSql = "update %s set %s %s" % (tbName,setValue[:-1],where)
         return self.excute(updateSql)
     # delete
     def delete(self,tbName, where):
@@ -61,7 +64,13 @@ class mysqlModel(object):
             print(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + '--' + str(e))
             return False
 
-    # close
+    # __close__
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
+    # __del__
+
     def __del__(self):
         self.cursor.close()
         self.conn.close()
